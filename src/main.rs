e6c50c1a0dcde47c05f9348a25c56053b89b8c0d
Bo <Bo>
2024-07-23 04:03:02 +0200

    First router

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("Hello world")})
+    );
+
+    // region:    --- Start server
+    let tcp_listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    println!("--> LISTENING  on {:?}", tcp_listener.local_addr().unwrap());
+    axum::serve(tcp_listener, routes_hello.into_make_service()).await.unwrap();
+    
 }
