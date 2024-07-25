45cf2c11c98eb7439c392157e82e7720fd1ac5e6
bo <rosenqvist9@telia.com>
2024-07-25 15:21:04 +0200

    Add tracing and env settings

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,18 @@ use axum::{middleware, Router};
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
 use tower_cookies::CookieManagerLayer;
+use tracing_subscriber::EnvFilter;
 
 // endregion: --- Modules
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        //.without_time()
+        .with_target(false)
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
     // Initialize ModelManager.
     let mm = ModelManager::new().await?;
 
