25633d558b802abe0a51a33bb09cb3a77d94b1d7
bo <rosenqvist9@telia.com>
2024-07-28 10:08:44 +0200

    Use trait From for model::Error

--- src/model/error.rs
+++ src/model/error.rs
@@ -1,54 +1,34 @@
 use crate::crypt;
 
 use super::store;
+use derive_more::From;
 use serde::Serialize;
 use serde_with::{serde_as, DisplayFromStr};
 
 pub type Result<T> = core::result::Result<T, Error>;
 
 #[serde_as]
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, From)]
 pub enum Error {
-    EntityNotFound { entity: &'static str, id: i64 },
+    EntityNotFound {
+        entity: &'static str,
+        id: i64,
+    },
 
     // -- Modules
+    #[from]
     Crypt(crypt::Error),
+    #[from]
     Store(store::Error),
 
     // -- Externals
+    #[from]
     Sqlx(#[serde_as(as = "DisplayFromStr")] sqlx::Error),
 
     // --seacquery
-    Sea(#[serde_as(as = "DisplayFromStr")] sea_query::error::Error),
+    #[from]
+    SeaQuery(#[serde_as(as = "DisplayFromStr")] sea_query::error::Error),
 }
-
-// region:    --- Froms
-
-impl From<store::Error> for Error {
-    fn from(value: store::Error) -> Self {
-        Self::Store(value)
-    }
-}
-
-impl From<sqlx::Error> for Error {
-    fn from(value: sqlx::Error) -> Self {
-        Self::Sqlx(value)
-    }
-}
-
-impl From<sea_query::error::Error> for Error {
-    fn from(value: sea_query::error::Error) -> Self {
-        Self::Sea(value)
-    }
-}
-
-impl From<crypt::Error> for Error {
-    fn from(value: crypt::Error) -> Self {
-        Self::Crypt(value)
-    }
-}
-
-// endregion: --- Froms
 
 // region:    --- Error Boilerplate
 impl core::fmt::Display for Error {
